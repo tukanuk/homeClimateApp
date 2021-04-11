c1720f0aecc8ab281957d18b0f9675b00c5d9188
benjamin <benjamin@idavidson.ca>
2021-04-11 16:52:52 -0400

    first commit

---
+++ homeClimateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeClimateApp
+//
+//  Created by Benjamin Davidson on 2021-04-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
