a77ec1fb2bdef63407815564bf63c7abf959e6ca
benjamin <benjamin@idavidson.ca>
2021-04-11 17:31:11 -0400

    Add sample test cases

--- homeClimateAppTests/homeClimateAppTests.swift
+++ homeClimateAppTests/homeClimateAppTests.swift
@@ -21,6 +21,8 @@ class homeClimateAppTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert("Test" == "Test")
+//        XCTAssert("Test" == "Tests")
     }
 
     func testPerformanceExample() throws {
