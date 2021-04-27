c8f927afcfc8e15823e5257f580098c2fe4c4117
Ben Davidson <benjamin@idavidson.ca>
2021-04-26 22:48:42 -0400

    Connected test data to detail view in app

--- homeClimateApp/Controller/ViewController.swift
+++ homeClimateApp/Controller/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var DetailText: UITextView!
+    
     var climateManager = ClimateManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let data = climateManager.loadJSON("Data/sampleData")
+        let results = climateManager.parseJSON(data!)
+        DetailText.text = results?[0].output()
+        
+    
+        print("Snark")
+        print(results)
     }
 
 
