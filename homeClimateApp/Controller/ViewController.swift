255efe6f858a7e2c65d3fb76d00a5b8abcf0975d
Ben Davidson <benjamin@idavidson.ca>
2021-04-30 07:17:29 -0400

    working on test data in the table view

--- homeClimateApp/Controller/ViewController.swift
+++ homeClimateApp/Controller/ViewController.swift
@@ -9,10 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    @IBOutlet weak var tableViewText: UITableView!
     @IBOutlet weak var DetailText: UITextView!
     
     var climateManager = ClimateManager()
+    let vegetables = ["Carrot", "Broccoli", "Cucumber"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,13 +21,45 @@ class ViewController: UIViewController {
         
         let data = climateManager.loadJSON("Data/sampleData")
         let results = climateManager.parseJSON(data!)
-        DetailText.text = results?[0].output()
+//
+        // create output
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, y HH:mm"
         
-    
-        print("Snark")
-        print(results)
+//        let detail : String = formatter.string(from: results![0].timeStamp) + "\n\(results![0].temperature)"
+        
+        var detailText = ""
+        for rec in results! {
+            detailText += "⏰ \(formatter.string(from: rec.timeStamp)) :: 🌡\(rec.temperature)C 💦\(rec.humidity)%\n"
+        }
+        
+        DetailText.text = detailText
     }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        tableViewText.reloadData()
+//    }
 
 
 }
 
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return vegetables.count
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Create a dynamic plain cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlainCell", for: indexPath)
+            
+//        cell.textLabel!.text = vegetables[indexPath.row]
+        cell.textLabel!.text = vegetables[indexPath.row]
+        
+        return cell
+        
+    }
+}
